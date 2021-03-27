69acff54d15013436a5b7a93490930645b77ca34
Matthew Courtney <matthewdc182@gmail.com>
2021-03-26 20:03:57 -0400

    finished off the quiz for boulder badge

---
+++ quizes.js
@@ -0,0 +1,52 @@
+var quizes = [
+
+    [
+        // Quiz1 Boulder Badge
+        {
+            title: "Digglet",
+            choices: ["Voltorb", "Digglet", "Hypno", "Tauros"],
+            answer: "Digglett"
+        },
+        {
+            title: "Sandshrew",
+            choices: ["Cubone", "Nidoran", "Sandshrew", "Oddish"],
+            answer: "Magikarp"
+        },
+        {
+            title: "Geodude",
+            choices: ["Graveler", "Slowbro", "Magneton", "Geodude"],
+            answer: "Geodude"
+        },
+        {
+            title: "Onix",
+            choices: ["Caterpie", "Onix", "Guarydos", "Dratini"],
+            answer: "Onix"
+        },
+        {
+            title: "Cubone",
+            choices: ["Cubone", "Marowak", "Hitmonchan", "Koffing"],
+            answer: "Cubone"
+        },
+
+    ],
+    
+
+]
+
+
+
+// {
+//     title: "Guarydos",
+//     choices: ["Caterpie", "Onyx", "Guarydos", "Dratini"],
+//     answer: "Guarydos"
+// },
+// {
+//     title: "Baulbasaur",
+//     choices: ["Ivysaur", "Metapod", "Venasaur", "Bulbasaur"],
+//     answer: "Dobby"
+// },
+// {
+//     title: "Magikarp",
+//     choices: ["Lapras", "Psyduck", "Vaporeon", "Magikarp"],
+//     answer: "Magikarp"
+// },
